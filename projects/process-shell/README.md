654cf3231a5fe00f18f5c0653ac0462148c249af
Ian Tower <25020681+i-tower@users.noreply.github.com>
2025-03-04 17:47:09 -0800

    process-shell now does the thing very simply 1 command only. No arguments

---
+++ projects/process-shell/README.md
@@ -0,0 +1,21 @@
+## A simple shell
+
+In this project we will implement a simple shell. Meaning a terminal command prompt from which we can run other programs. The shell should operate in 2 modes.  Interactive and Batch.
+
+The shell should be able to run programs found in the systems path. Eg. ls -la
+
+The shell should exit when a user enters exit
+
+The default path should contain "/bin"
+
+The user should be able to cd to different directories
+
+
+The user should be able to run commands in parallel 
+
+```
+
+wish> cmd1 & cmd2 args1 args2 & cmd3 args1
+
+```
+
--- projects/process-shell/wish.c
+++ projects/process-shell/wish.c
@@ -1,2 +1,74 @@
-#include <stdio.h>
+#define _POSIX_C_SOURCE 200809L
+#define _GNU_SOURCE
 
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#define MAX_ARGS 10
+#define MAX_ARG_LEN 256
+
+
+
+
+int main(void) {
+    
+    char* line_ptr;
+    char* path;
+    size_t line_len = 0;
+    ssize_t nread = 0;
+
+    //HACK: Eventually handle this with arg parsing library.
+    // should return a null terminated array of char*s 
+    // first entry is file name subsequent entries are arguments
+    char *arg_list[10];
+    char arg_buffer[256];    
+    
+    while(1) {
+        
+        if ((nread= getline(&line_ptr, &line_len, stdin)) < 0) {
+            exit(1);
+        } else if (nread > MAX_ARG_LEN) {
+            fprintf(stderr, "Input too long\n");
+            exit(1);
+        }
+        // TODO: Extract command line arguments
+        //HACK: Eventually hand the string to the argument parser
+        //Set the newline character to 0; FIXME: Does this work with eof? 
+        line_ptr[nread - 1] = '\0';
+
+        //HACK: Should I use length strings? Perhaps the parsing lib should but I think
+        // the top level program should use standard c strings?
+
+        memcpy(arg_buffer, line_ptr, nread);
+        arg_list[0] = arg_buffer;
+        arg_list[1] = NULL; 
+
+        //HACK: Do path things
+        if((asprintf(&path,"/usr/bin/%s%c", arg_buffer, '\0')) < 0){
+            fprintf(stderr, "Allocation failed in asprintf for path\n");
+        }
+        
+        printf("Path is %s\n", path);
+
+        if (fork() == 0) {
+            // We are the child
+            if((execv(path, arg_list)) < 0) {
+                perror("execv");
+                exit(1);
+            }
+            
+        } else {
+            // Parent process
+            wait(NULL);
+        }
+
+    }
+
+    free(line_ptr);
+    free(path);
+    return 0;
+}
