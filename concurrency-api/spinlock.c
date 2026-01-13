882fe5f90e931757761d40672ac1aea40e1e4506
Ian Tower <25020681+i-tower@users.noreply.github.com>
2026-01-12 17:06:19 -0800

    Experiment with creating a spinlock

--- concurrency-api/spinlock.c
+++ concurrency-api/spinlock.c
@@ -1,16 +1,35 @@
 #include <stdio.h>
 
-int lock = 0; 
-
-static inline int xchg(volatile int *addr, int newval)
-{
-    int result
-   
+int lock_acquire(int* lock) {
+    while (*lock);
+   *lock = 1;
+  return 1;
 }
 
-int main(void) 
-{
-
+int lock_release(int* lock) {
+    *lock = 0;
     return 0;
 }
 
+struct someval {
+    int lock;
+    int value;
+};
+
+int main(void)
+{
+    struct someval lockedval = {
+        .lock = 0,
+        .value = 225,
+    };
+
+    printf("Locked value before %d\n", lockedval.value);
+
+    lock_acquire(&lockedval.lock);
+    lockedval.value = 0;
+    lock_release(&lockedval.lock);
+
+    printf("Locked value after %d\n", lockedval.value);
+
+    return 0;
+}
